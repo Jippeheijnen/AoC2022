d589e1645336513e1ec6ee9d18ede8ae87769f83
Jippe Heijnen <jippe.heijnen@student.hu.nl>
2022-12-04 12:58:58 +0100

    create day 4 and started defining function

---
+++ day4/main.py
@@ -0,0 +1,18 @@
+"""
+
+(c) Jippe Heijnen 2022
+main.py created at 04/12/2022
+Distributed under the Boost Software License v1.0.
+(See license at https://www.boost.org/LICENSE_1_0.txt)
+
+"""
+
+from typing import List
+from helpers import *
+
+def allInefficientPairsN(input: List[str]) -> int:
+    for pair in input:
+
+
+if __name__ == '__main__':
+    print(allInefficientPairsN(getInput('input.txt')))
