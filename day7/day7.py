852b055c1971d8604d8e8045ba26dff4815baa51
Jippe Heijnen <jippe.heijnen@student.hu.nl>
2022-12-07 12:20:04 +0100

    current progress on day 7

---
+++ day7/day7.py
@@ -0,0 +1,81 @@
+"""
+
+(c) Jippe Heijnen 2022
+day7.py created at 07/12/2022
+Distributed under the Boost Software License v1.0.
+(See license at https://www.boost.org/LICENSE_1_0.txt)
+
+"""
+
+from enum import Enum
+from typing import List
+
+
+class type(Enum):
+    """
+    A little class to distinguish types from eachother
+    """
+
+    # folder stuff
+    file = 0
+    dir = 1
+    # command stuff
+    cd = 2
+    ls = 3
+
+class Command:
+    def __init__(self, type: type):
+        self.type: type = type
+
+class CD_Command(Command):
+    def __init__(self, dir_to_go_to: str):
+        super().__init__(type.cd)
+        self.dir_to_go_to: str = dir_to_go_to
+        
+    def __repr__(self):
+        return f'$ cd {self.dir_to_go_to}'
+
+class LS_Command(Command):
+    def __init__(self, returns: List[str]):
+        super().__init__(type.ls)
+        self.returns: List[str] = returns
+    
+    def __repr__(self):
+        return f'$ ls'
+    
+class FS_Item():
+    def __init__(self, type: type, name: str, size: int = 0):
+        self.type = type
+        self.name:str = name
+        self.size = size
+        
+    def __repr__(self):
+        return f'{"dir" if self.type else "file"} {self.name} {"/" if self.type else self.size}'
+
+class Tree_node:
+    def __init__(self,
+                 type: type,
+                 name: str,
+                 size: int = 0,
+                 parent: 'Tree_node' = None):
+        self.__type: type = type
+        self.__size: int = size
+        self.__name: str = name
+        self.__parent: Tree_node = parent
+        self.__items: List[FS_Item] = []
+        self.__children: List[Tree_node] = []
+
+    def __repr__(self) -> str:
+        return self.__name
+
+    def addChild(self, child: 'Tree_node'):
+        self.__children.append(child)
+        
+    def addItem(self, item: FS_Item):
+        self.__items.append(item)
+
+    def getChildren(self) -> List['Tree_node']:
+        return self.__children
+
+    def getSize(self):
+        return self.__size
---
+++ day7/main.py
@@ -0,0 +1,95 @@
+"""
+
+(c) Jippe Heijnen 2022
+main.py created at 07/12/2022
+Distributed under the Boost Software License v1.0.
+(See license at https://www.boost.org/LICENSE_1_0.txt)
+
+"""
+from helpers import *
+from day7 import *
+from typing import List, Union
+
+
+def folder_exists_in_fs(root_node: Tree_node, folder_dir: str) -> bool:
+    path: List[str] = folder_dir.split('/')
+    folder_exists: bool = False
+
+    # todo: implement this
+
+    # if len(root_node.getChildren()) != 0:
+
+    return folder_exists
+
+
+def processInput(input: List[str]) -> List[Command]:
+    commands: List[Command] = []
+    command_list_to_add: List[str] = []
+    cur_dir: str = ''
+    # need offset to remember the correct loop iterations
+    i: int = 0
+    command_list_to_add.append(input[i])
+    try:
+        while not input[i+1][0] == '$':
+            i+=1
+            command_list_to_add.append(input[i])
+            continue
+        return processInput(input[i+1:]) + [createCommand(command_list_to_add)]
+    except IndexError:
+        return [createCommand(command_list_to_add)]
+
+
+
+def createCommand(command: List[str]) -> Union[CD_Command, LS_Command]:
+    # dc = don't care because it's '$'.
+    #  check for parameterless command
+    if len(command[0].split()) == 2:
+        dc, comm = command[0].split()
+    else:
+        dc, comm, path = command[0].split()
+    if comm == 'cd':
+        return CD_Command(path)
+    elif comm == 'ls':
+        # check if ls returned any items
+        if not len(command) > 1:
+            return LS_Command(list(None))
+        else:
+            return LS_Command(command[1:])
+
+
+def createNodes(commands: List[Union[CD_Command, LS_Command]], root_node: Tree_node = Tree_node(type.dir, 'root'), working_dir: List[str] = []) -> Tree_node:
+    new_node: Tree_node = None
+    if commands[0].type == type.cd:
+        if not commands[0].dir_to_go_to == '..':
+            new_node: Tree_node = Tree_node(type.dir, commands[0].dir_to_go_to, parent=root_node)
+            working_dir.append(commands[0].dir_to_go_to)
+        else:
+            new_node: Tree_node = Tree_node(type.dir, working_dir.pop(), parent=root_node)
+            root_node.addChild(new_node)
+            return createNodes(commands[1:], new_node)
+    elif commands[0].type == type.ls:
+            values: List[str] = commands[0].returns
+            for i in range(len(values)):
+                item: List[str] = values[i].split()
+                name: str
+                size: int
+                # starts with number so we're adding a file here
+                if values[i][0].isdigit():
+                    size, name = item
+                    root_node.addItem(FS_Item(type.file, name, size))
+                # Directories added here
+                else:
+                    dc, name = item
+                    root_node.addItem(FS_Item(type.dir, name))
+    # shave first command off the list
+    return createNodes(commands[1:], new_node)
+    
+            
+
+
+if __name__ == '__main__':
+    command_list: List[Union[CD_Command, LS_Command]] = processInput(getInput('input.txt'))
+    command_list.reverse()
+    root: Tree_node = \
+        createNodes(command_list)
+    print(f' size of root: {root.getSize()}')
